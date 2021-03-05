2a9c1272350653d74b0d9d27d141c134e49a7937
Jarno Luoma-Nirva <jarno.luoma-nirva@vaisto.io>
2021-03-05 21:30:19 +0200

    Derive choice from title

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,11 @@
 import { createStore } from 'vuex'
 import { State } from '@/types'
 
+function idFromTitle(title: string): string {
+  let newId: string = title.toLowerCase()
+  newId = newId.replace(' ', '_')
+  return newId
+}
 
 export default createStore({
   state () {
@@ -25,12 +30,18 @@ export default createStore({
   },
   mutations: {
     addChoice (state: State, title: string) {
-      const id: number = state.choices.length
-      const color = state.colors[id]
-      state.choices.push({ id: id.toString(), title: title, color: color })
+      const colorIndex: number = state.choices.length
+      const color = state.colors[colorIndex]
+      const newId: string = idFromTitle(title)
+      // TODO: Check that choice with this ID does not yet exist
+      console.log('addChoice id:', newId, 'title:', title, 'color:', color)
+      state.choices.push({ id: newId, title: title, color: color })
     },
     clearChoices (state: State) {
       state.choices = []
+    },
+    setPollId (state: State, pollId: string) {
+      state.pollId = pollId
     }
   },
   actions: {},
