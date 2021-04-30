e9e59aff983a67925c6f619bae29b7910cbd891f
Jarno Luoma-Nirva <jarno.luoma-nirva@vaisto.io>
2021-05-01 00:20:23 +0300

    Use actual backend

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { Ballot, Choice, Poll } from '@/types'
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://wtgfl-backend.onrender.com/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
